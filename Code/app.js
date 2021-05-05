03fd3b5b63ebe3e4f6a9af913915068914fac10a
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-05 10:49:07 +0000

    Prevent users from accessing static files

--- Code/app.js
+++ Code/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var landingRouter = require('./routes/landingRouter');
+var appRouter = require('./routes/appRouter');
 
 var app = express();
 
@@ -17,10 +17,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Serve static files
+app.use('/styles', express.static(path.join(__dirname, 'public', 'styles')));
+app.use('/scripts', express.static(path.join(__dirname, 'public', 'scripts')));
+app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
+
+app.use('/', landingRouter);
+app.use('/app', appRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
