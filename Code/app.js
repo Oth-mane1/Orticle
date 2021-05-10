b69db890a3de134a330a427b23972f10d9d3d0b4
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-10 23:01:33 +0000

    Install passport Js

--- Code/app.js
+++ Code/app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const passport = require('passport');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var landingRouter = require('./routes/landingRouter');
-var appRouter = require('./routes/appRouter');
+const landingRouter = require('./routes/landingRouter');
+const appRouter = require('./routes/appRouter');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
