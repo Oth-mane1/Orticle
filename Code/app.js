e1da3dcdf4e1556abdbf994e56e410098d57d539
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-11 01:28:12 +0000

    Remove passport JS

--- Code/app.js
+++ Code/app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
-const passport = require('passport');
+const session = require('express-session');
+const fileStore = require('session-file-store')(session);
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -9,6 +10,13 @@ const landingRouter = require('./routes/landingRouter');
 const appRouter = require('./routes/appRouter');
 
 const app = express();
+app.use(session({
+    name: 'Orticle-Session',
+    secret: 'orticle-secret-sesseion',
+    saveUninitialized: false,
+    resave: false,
+    store: fileStore()
+}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,19 +36,19 @@ app.use('/', landingRouter);
 app.use('/app', appRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
