3520ae8d77a35389ba0582a0ccd1cbbdb0c01bf5
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-14 23:26:50 +0000

    Render explore view

--- Code/routes/exploreRouter.js
+++ Code/routes/exploreRouter.js
@@ -5,8 +5,7 @@ const router = express.Router();
 router.route('/')
     .get((req, res, next) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.sendFile(path.resolve('public', 'app', 'explore.html'));
+        res.render('explore', { name: req.session.username });
     });
 
 module.exports = router;
