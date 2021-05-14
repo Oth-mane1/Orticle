81cde36a533cf0ab58e5736e43a3727db09b2cf1
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-14 01:39:06 +0000

    Simplify syntax

--- Code/routes/exploreRouter.js
+++ Code/routes/exploreRouter.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(path.resolve('public', 'app', 'explore.html'));
-});
+router.route('/')
+    .get((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.sendFile(path.resolve('public', 'app', 'explore.html'));
+    });
 
 module.exports = router;
