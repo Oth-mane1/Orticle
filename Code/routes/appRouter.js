5704c030a32da44de125904c0dbf7f92f10c4e6f
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-14 01:36:38 +0000

    Redirect user if not connected

--- Code/routes/appRouter.js
+++ Code/routes/appRouter.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const exploreRouter = require('./exploreRouter');
 const userRouter = require('./userRouter');
+const accountRouter = require('./accountRouter');
 const router = express.Router();
+
+// If user not authenticated redirect to signIn
+router.use((req, res, next) => {
+    if (!req.session.username) {
+        res.statusCode = 401;
+        return res.redirect('/signIn');
+    }
+    next()
+})
+
+// Set-up explore as the root of the app
+router.route('/').get((req, res) => {
+    res.redirect('/app/explore');
+})
 
 router.use('/explore', exploreRouter);
 router.use('/user', userRouter);
+router.use('/account', accountRouter);
 
 module.exports = router;
