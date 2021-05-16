1a74ad25b6c30886c343c773c93c9fc8ce1e0800
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-16 01:46:48 +0000

    Add new route

--- Code/routes/articleRouter.js
+++ Code/routes/articleRouter.js
@@ -10,15 +10,7 @@ const router = express.Router();
 router.route('/')
     .get((req, res, next) => {
         // TODO: Render pages with user article
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.sendFile(path.resolve('public', 'app', 'article.html'));
-    })
-
-/* GET article by id */
-router.route('/:id')
-    .get((req, res, next) => {
-        const idArt = req.params.id
+        const userid = req.session.userid;
 
         const pool = new sql.ConnectionPool(dbConfig)
         pool.connect(err => {
@@ -29,24 +21,24 @@ router.route('/:id')
             }
 
             var request = new sql.Request(pool);
-            request.input('id', idArt);
-            request.execute('getArticle', (err, recordsets) => {
+            request.input('IdUtl', userid);
+            request.input('sourceArt', source);
+            request.input('titreArt', title);
+            request.input('extraitArt', extrait);
+            request.execute('createArticle', (err, recordsets) => {
                 if (err) {
                     console.log(err);
                     res.statusCode = 500;
                     return res.end();
                 }
 
-                if (!recordsets.recordset[0]) {
-                    res.statusCode = 404;
-                    return res.end(`Article ${idArt} introuvable`)
-                }
-
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(recordsets.recordset[0]);
+                res.statusCode = 201;
+                res.end('l\'Article a bien ete creer')
             });
         })
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.sendFile(path.resolve('public', 'app', 'article.html'));
     })
 
 /* GET add article Page */
@@ -90,9 +82,43 @@ router.route('/add')
 router.route('/getContent')
     .get((req, res, next) => {
         const { link } = req.body;
-        Mercury.parse(link, {contentType : 'text'}).then((result) => {
-            console.log(result)
-            res.end()
+        Mercury.parse(link, { contentType: 'text' }).then((result) => {
+            res.statusCode = 200;
+            res.json({ title: result.title, content: result.content })
+        })
+    })
+
+/* GET article by id */
+router.route('/get/:id')
+    .get((req, res, next) => {
+        const idArt = req.params.id
+
+        const pool = new sql.ConnectionPool(dbConfig)
+        pool.connect(err => {
+            if (err) {
+                console.log(err)
+                res.statusCode = 401;
+                return
+            }
+
+            var request = new sql.Request(pool);
+            request.input('id', idArt);
+            request.execute('getArticle', (err, recordsets) => {
+                if (err) {
+                    console.log(err);
+                    res.statusCode = 500;
+                    return res.end();
+                }
+
+                if (!recordsets.recordset[0]) {
+                    res.statusCode = 404;
+                    return res.end(`Article ${idArt} introuvable`)
+                }
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(recordsets.recordset[0]);
+            });
         })
     })
 
