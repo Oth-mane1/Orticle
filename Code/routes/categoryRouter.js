807ca5a2bc07475208f9c8b5af0525e112a00c7f
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-17 03:56:56 +0100

    Add category router

---
+++ Code/routes/categoryRouter.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const path = require('path');
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+
+const router = express.Router();
+
+/* GET category page */
+router.route('/')
+    .get((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.sendFile(path.resolve('public', 'app', 'category.html'));
+    });
+
+/* GET each category page */
+router.route('/:idcat')
+    .get((req, res, next) => {
+        const idcat = req.params.idcat;
+
+        const pool = new sql.ConnectionPool(dbConfig)
+        pool.connect(err => {
+            if (err) {
+                console.log(err)
+                res.statusCode = 401;
+                return
+            }
+            var request = new sql.Request(pool);
+            request.input('id', idcat);
+            request.execute('getCategory', (err, recordsets) => {
+                if (err) {
+                    console.log(err);
+                    res.statusCode = 500;
+                    res.end();
+                    return
+                }
+
+                if (recordsets.recordset.length === 0) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end('categorie ne contient aucune orticle');
+                    return
+                }
+                
+                let ispushed = false
+                recordsets.recordsets[1].forEach(ort => {                    
+                    ort.idee = new Array()
+                });
+
+                recordsets.recordsets[1].forEach(ort => {                    
+                    recordsets.recordsets[2].forEach(idee => {
+                        if (idee.IdOrt === ort.IdOrt) {
+                            ort.idee.push(idee)
+                            ispushed = true
+                        }
+                        else {
+                            if (ispushed) {
+                                return
+                            }
+                            ispushed = false
+                        }
+                    });
+                });
+
+                const catInfos = {
+                    title: recordsets.recordsets[0][0].nomCat,
+                    orticle: recordsets.recordsets[1]
+                }
+                // console.log(catInfos)
+                // console.log(catInfos.orticle[3].idee)
+                res.statusCode = 200;
+                res.render('showCategory', { catInfos });
+            });
+        })
+    });
+
+module.exports = router;
