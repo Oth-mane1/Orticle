f112a7dfca03c7904860f50cb6f7fe33919d7ab2
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-17 03:55:49 +0100

    implement category router

--- Code/routes/appRouter.js
+++ Code/routes/appRouter.js
@@ -3,6 +3,7 @@ const exploreRouter = require('./exploreRouter');
 const userRouter = require('./userRouter');
 const orticleRouter = require('./orticleRouter');
 const articleRouter = require('./articleRouter');
+const categoryRouter = require('./categoryRouter');
 const accountRouter = require('./accountRouter');
 const router = express.Router();
 
@@ -23,6 +24,7 @@ router.route('/').get((req, res) => {
 router.use('/explore', exploreRouter);
 router.use('/orticle', orticleRouter);
 router.use('/article', articleRouter);
+router.use('/category', categoryRouter);
 router.use('/user', userRouter);
 router.use('/account', accountRouter);
 
