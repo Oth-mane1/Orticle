661a3280f43b830fb721b825bb2d8a004f77231b
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-18 01:18:52 +0100

    Fix bug in err handler

--- Code/app.js
+++ Code/app.js
@@ -66,14 +66,14 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use(async (err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    return await res.render('error');
 });
 
 module.exports = app;
