7ef6872a470459a977f624d1ec5e7f8c5dd516da
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-19 02:19:50 +0100

    Render the page with user last name

--- Code/routes/exploreRouter.js
+++ Code/routes/exploreRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.route('/')
     .get((req, res, next) => {
         res.statusCode = 200;
-        res.render('explore', { name: req.session.username });
+        res.render('explore', { name: req.session.userlastn });
     });
 
 module.exports = router;
