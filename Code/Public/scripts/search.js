35d5fc550d607a8021cb57d08e8be8a6eb975fc8
Othmane Ouirdy <othmane.ouirdy@gmail.com>
2021-05-25 23:15:14 +0100

    Add roulling spin before the result appear

--- Code/Public/scripts/search.js
+++ Code/Public/scripts/search.js
@@ -10,12 +10,18 @@ search.addEventListener('click', (e) => {
         swal.fire("Attention", "Veuillez Entrer ce que vous voulez chercher!", "warning")
     }
     else {
+        searchResult.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="width: 100px; height:100px; margin: auto; background: none; display: block; shape-rendering: auto;" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+        <circle cx="50" cy="50" fill="none" stroke="#397BA7" stroke-width="10" r="32" stroke-dasharray="150.79644737231007 52.26548245743669">
+          <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1.8518518518518516s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
+        </circle>
+        </svg>`
+
         fetch(`http://localhost:5000/app/search/${index.options[index.selectedIndex].value}/`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({                
+            body: JSON.stringify({
                 text: searchText.value
             })
         }).then((res) => {
@@ -29,6 +35,7 @@ search.addEventListener('click', (e) => {
             }
         }).catch((error) => {
             swal.fire("Erreur", "Une erreur s'est produit au niveau du serveur veuillez réessayer plus tard", "error")
+            searchResult.innerHTML = null
         })
     }
 })
