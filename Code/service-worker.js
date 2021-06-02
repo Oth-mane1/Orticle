ad41f3cc98d7de818415b51749191c93b8cbefbc
Oth-mane1 <othmane.ouirdy@gmail.com>
2021-06-02 19:54:26 +0100

    Upgrade to version v1.1.0

--- Code/service-worker.js
+++ Code/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "OrticleV1.0.0";
+const CACHE_NAME = "OrticleV1.1.0";
 
 const cache = async (request, response) => {
     if (response.type === "error" || response.type === "opaque") {
